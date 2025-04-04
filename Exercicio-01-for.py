abc021d2882b29a6c4f3356e5bdea90dc3b94cba
Helder <hcbfilho@hotmail.com>
2025-04-03 23:17:50 -0300

    exercicio 1

--- Exercicio-01-for.py
+++ Exercicio-01-for.py
@@ -1,14 +1,18 @@
 
 qtd_alunos = int(input('Insira a quantidade de alunos '))
-notas_alunos = {}
+soma_media = 0
 
 for i in range(1,qtd_alunos + 1):
     qtd_notas = int(input(f'Digite a quantidade de notas do {i}º aluno '))
-    notas_alunos[i] = [] 
+    nota = 0
+
     for a in range(1, qtd_notas + 1):
         notas = float(input(f'Insira a {a}ª nota do aluno '))
-        notas_alunos[i].append(notas)
-        
-print("\nNotas dos alunos:")
-for aluno, notas in notas_alunos.items():
-    print(f"Aluno {aluno}: {notas}")
+        nota += notas
+        media = nota/qtd_notas
+
+    print(f'A média do {i}º aluno é: {media:.2f}')
+    soma_media += media
+    media_turma = soma_media / qtd_alunos
+
+print(f'A média geral da turma é: {media_turma:.2f}')
